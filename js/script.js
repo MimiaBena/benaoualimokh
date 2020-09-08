7aaa33b8a75c957cf6b3cc88ef1fa578f566ea1d
MimiaBena <38703774+MimiaBena@users.noreply.github.com>
2020-09-08 19:31:32 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+$(function(){
+
+    $(".navbar a, footer a").on("click", function(event){
+    
+        event.preventDefault();
+        var hash = this.hash;
+        
+        $('body,html').animate({scrollTop: $(hash).offset().top} , 900 , function(){window.location.hash = hash;})
+        
+    });
+    
+    $('#contact-form').submit(function(e){
+     e.preventDefault();
+     $('.commentaire').empty();
+     var postdata = $('#contact-form').serialize();
+     
+      $.ajax({
+          type: 'POST',
+          url: 'php/contact.php',
+          data: postdata,
+          dataType: 'json',
+          success: function(result){
+              
+              if(result.isSuccess){
+                  $("#contact-form").append("<p class='thank-you'>Votre message a bien été envoyé.Merci de m'avoir contacté :)</p>");
+                  $("#contact-form")[0].reset();
+                  
+              }
+              else{
+                  $("#firstname + .commentaire").html(result.firstnameError);
+                  $("#name + .commentaire").html(result.nameError);
+                  $("#email + .commentaire").html(result.emailError);
+                  $("#phone + .commentaire").html(result.phoneError);
+                  $("#message + .commentaire").html(result.messageError);
+              }
+          }
+          
+          
+          
+      });
+     
+     
+ });
+    
+
+})
